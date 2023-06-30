5a38c03afa61bfe67b378fe1f25c2ef370884efc
Pedro Luan <macielpedro794@gmail.com>
2023-06-30 14:59:09 -0300

    barra de pesquisa funcionando

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,17 +1,22 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
-import CardPokemon from "../Components/CardPokemon"
-import { Container, Row, Col } from "react-bootstrap"
-import { getIdPokemon, getNamePokemon, getPokemonTypeInPortuguese, getTypes } from "../ExportFunctions/ExportFunctions"
-import { Card } from 'react-bootstrap'
-import '../Css/CardPokemon.css'
-import { getPokemonColorByType } from '../ExportFunctions/ExportFunctions'
+import { Container, Row, Col, Card } from "react-bootstrap"
+import {
+  getIdPokemon,
+  getNamePokemon,
+  getPokemonTypeInPortuguese,
+  getPokemonColorByType
+} from "../ExportFunctions/ExportFunctions"
 import { Link } from "react-router-dom"
+import PokemonLogo from '../Images/pokemonLogo.png'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../Css/CardPokemon.css'
+import '../Css/SearchBar.css'
 
 
 export default function App() {
   const [dataPokemon, setDataPokemon] = useState<any>([])
+  const [search, setSearch] = useState('')
   async function getPokemons() {
     let endpoints = []
     for (let i = 1; i <= 12; i++) {
@@ -24,23 +29,40 @@ export default function App() {
   }
 
 
+  const cardFilter = dataPokemon.filter((card: any) =>
+    card.name.includes(search.toLowerCase()) ||
+    card.id.toString().includes(search.toString())
+  )
+
   useEffect(() => {
     getPokemons()
   }, [])
 
   return (
     <>
+      <div className="seacrhBar">
+        <div>
+          <img src={PokemonLogo} className="logoPokemon" />
+        </div>
+        <div className="searchContainer">
+          <input
+            type='text'
+            value={search}
+            onChange={(e) => (setSearch(e.target.value))}
+          />
+        </div>
+      </div>
       <Container fluid >
         <Row>
-          {dataPokemon.map((data: any, key: any) => {
+          {cardFilter.map((data: any, key: any) => {
             return (
               <Col lg='2' md='4' sm='6' xs='12' key={key}>
                 <Link to={`/profile/${data.id}`} style={{ textDecoration: "none" }}>
                   <Card className="CardPokemonData">
                     <Card.Body>
-                      <Card.Text className="bodyColorType" id="bodyColorType" style={{ backgroundColor: `${getPokemonColorByType(data.types[0].type.name)}` }}>{data.types.map((type: any) => {
+                      <Card.Text className="bodyColorType" id="bodyColorType" style={{ backgroundColor: `${getPokemonColorByType(data.types[0].type.name)}` }}>{data.types.map((type: any, key: any) => {
                         return (
-                          <div style={{ margin: '0 7px' }}>{getPokemonTypeInPortuguese(type.type.name)}</div>
+                          <div key={key} style={{ margin: '0 7px' }}>{getPokemonTypeInPortuguese(type.type.name)}</div>
                         )
                       })}</Card.Text>
                       <div className="pokemonNameId">{getNamePokemon(data)} <strong className='pokemonId'>#{getIdPokemon(data)}</strong> </div>
